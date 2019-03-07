01edaa1e4c793193611de70fb873c1a773d42cae
Saumye Malhotra <saumymemalhotra94@gmail.com>
2019-03-07 17:33:51 +0530

    minimal bot

--- app.js
+++ app.js
@@ -3,8 +3,27 @@ var config   = require('config');
 
 var bot = new Telegraf(config.get('botToken'));
 
-bot.start((ctx) => ctx.reply('Welcome!'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.launch()
+bot.start((ctx) => ctx.reply('Hi ' + ctx.message.from.first_name + ', Which batch are you from?(YYYY fromat)'));
+
+// bot.help((ctx) => ctx.reply('Send me a sticker'))
+// bot.on('sticker', (ctx) => ctx.reply('👍'))
+// bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+
+bot.on('new_chat_members', (ctx) => {
+    console.log("New chat members =>", ctx.message.new_chat_members);
+    var newMembers = ctx.message.new_chat_members;
+    var membersFirstName = "";
+    for (var i in newMembers) {
+        if (i > 0) {
+            membersFirstName += ", "
+        }
+        membersFirstName += newMembers[i].first_name;
+    }
+    var message = "Hi " + membersFirstName
+    + ". Welcome to the NITK alumni group! Please introduce yourself to me, by clicking, @" + config.get('botName')
+    ctx.reply(message);
+})
+
+bot.on('')
+
+bot.launch();
