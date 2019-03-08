af5a83284838a1864d1b84169ff2e6632363f826
Saumye Malhotra <saumymemalhotra94@gmail.com>
2019-03-08 11:28:29 +0530

    removed pakage-lock. added mongo init func

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-var Telegraf = require('telegraf');
-var config   = require('config');
+var Telegraf    = require('telegraf');
+var config      = require('config');
+var MongoClient = require('mongodb').MongoClient;
 
 var bot = new Telegraf(config.get('botToken'));
 
-bot.start((ctx) => ctx.reply('Hi ' + ctx.message.from.first_name + ', Which batch are you from?(YYYY fromat)'));
+bot.start((ctx) => ctx.reply('Hi ' + ctx.message.from.first_name + ', I suppose you are a NIT Kurukshetra Alumnus.' 
++ 'Which batch are you from?(YYYY fromat)'));
 
-// bot.help((ctx) => ctx.reply('Send me a sticker'))
-// bot.on('sticker', (ctx) => ctx.reply('👍'))
-// bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.help((ctx) => ctx.reply('Hi am ' + config.get('botName') + ". I help introduce alumni of NIT Kurukshetra to each other"));
 
 bot.on('new_chat_members', (ctx) => {
-    console.log("New chat members =>", ctx.message.new_chat_members);
-    var newMembers = ctx.message.new_chat_members;
+    console.log("New chat members =>", ctx && ctx.message && ctx.message.new_chat_members);
+    var newMembers = ctx && ctx.message && ctx.message.new_chat_members;
     var membersFirstName = "";
     for (var i in newMembers) {
         if (i > 0) {
@@ -24,6 +24,36 @@ bot.on('new_chat_members', (ctx) => {
     ctx.reply(message);
 })
 
-bot.on('')
+bot.on('text', (ctx) => {
+    console.log("message =>", ctx.update.message);
+    if (ctx && ctx.update && ctx.update.message) {
+        var messageRecieved = ctx.update.message;
+        var textMsg = messageRecieved.text;
+        var from = messageRecieved.from;
+        var fromId = from.id;
+        //have an array of questions, ask them in order
+        //and based NER or regex mark them answered,
+        //ask next question from unanswered set
+        if (messageRecieved.chat.type == 'private') {
+            //reply only if private chat
+        }
+    }
+})
 
 bot.launch();
+
+function startInitialProcess() {
+    MongoClient.connect(config.get('databaseSettings.database'), function(err, database) {
+      db = ''
+      if (!err) {
+        console.log("Database initialized");
+        db = database;
+        dbo = db.db(config.get('databaseSettings.name'))
+      } else {
+        console.error("Error while connecting to database");
+        throw err;
+      }
+    })
+}
+  
+startInitialProcess();
