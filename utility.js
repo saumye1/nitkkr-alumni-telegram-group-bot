17903873a23e22246ff7d70650f8c9eff693eeed
Saumye Malhotra <saumymemalhotra94@gmail.com>
2019-03-11 12:59:45 +0530

    deletion fix

--- utility.js
+++ utility.js
@@ -23,32 +23,32 @@ function sendMessage(ctxObj, message, type, chatType) {
 
 //delete only, not private, not introductory messages, not deleted
 function deleteUnnecessaryMessages(bot) {
-    dbo.collection(config.get('mongoCollections.messageLogs')).find(
-        {
-            is_deleted : false, 
-            type : {$ne : 'introductory'},
-            chat_type : {$ne : 'private'}
-        }).toArray( function(err, result) {
-            if (!err && result && result.length) {
-                var messagesInfo = result.map(message => {return message.message_info});
-                for (var i in messagesInfo) {
-                    bot.telegram.deleteMessage(messagesInfo[i].chat.id, messagesInfo[i].message_id);
-                }
-                var ids = result.map(message => {return message._id});
-                console.log("The following IDs were deleted:\n", JSON.stringify(ids));
-                dbo.collection(config.get('mongoCollections.messageLogs')).update(
-                {
-                    is_deleted : false, 
-                    type : {$ne : 'introductory'},
-                    chat_type : {$ne : 'private'}
-                }, {$set : {is_deleted : true}}, {multi : true},
-                    function(error, res) {
-                        if (error) {
-                            console.error(error);
-                        } else {
-                            console.log("All updated deleted smoothely");
-                        }
-                    })
+    var deletionTime = new Date().getTime();
+    var condition = {
+        is_deleted : false, 
+        type : {$ne : 'introductory'},
+        chat_type : {$ne : 'private'},
+        datetime : {$lt : deletionTime - config.get('deletePeriod')}
+    };
+    dbo.collection(config.get('mongoCollections.messageLogs')).find(condition).toArray( function(err, result) {
+        console.log("Finding messages that have been more than " 
+        + config.get('deletePeriod') / (1000 * 60 * 60) + " hours old", result && result.length);
+        if (!err && result && result.length) {
+            var messagesInfo = result.map(message => {return message.message_info});
+            for (var i in messagesInfo) {
+                bot.telegram.deleteMessage(messagesInfo[i].chat.id, messagesInfo[i].message_id);
             }
-        }) 
+            var ids = result.map(message => {return message._id});
+            console.log("The following IDs were deleted:\n", JSON.stringify(ids));
+            dbo.collection(config.get('mongoCollections.messageLogs')).update(condition,
+                {$set : {is_deleted : true}}, {multi : true},
+                function(error, res) {
+                    if (error) {
+                        console.error(error);
+                    } else {
+                        console.log("All updated deleted smoothely");
+                    }
+                })
+        }
+    }) 
 }
